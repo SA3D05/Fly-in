dda75d812c07e0281b63a7c5eb59294c7f3a9cf1
saad atifi <saadworkdotcom@gmail.com>
2026-04-09 14:32:19 +0100

    fix metadata parser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from enum import Enum
 
 map_data: dict = {
     "hub": [],
@@ -6,18 +7,96 @@ map_data: dict = {
 }
 
 
-def parse_metadata(metadata: list) -> dict:
-    result: dict = {}
-    print("parse_metadata,", metadata)
-    for data in metadata:
-        stripped_data = data.strip("[]")
-        splitted_data = stripped_data.split("=")
-        result.update({splitted_data[0]: splitted_data[1]})
+class ZoneType(Enum):
+    Normal = "normal"
+    Blocked = "blocked"
+    Restricted = "restricted"
+    Priority = "priority"
+
+
+class Color(Enum):
+    red = "red"
+    blue = "blue"
+    gray = "gray"
+    green = "green"
+
+
+def valid_metadata(tag: str, value: str):
+
+    PARSERS = {
+        "zone": {
+            "normal": ZoneType.Normal,
+            "blocked": ZoneType.Blocked,
+            "restricted": ZoneType.Restricted,
+            "priority": ZoneType.Priority,
+        },
+        "color": {
+            "red": Color.red,
+            "blue": Color.blue,
+            "gray": Color.gray,
+            "green": Color.green,
+        },
+    }
+
+    try:
+        if tag == "max_drones":
+            return int(value)
+        return PARSERS[tag][value]
+    except KeyError:
+        raise ValueError(f"tag or value not valid '{tag} = {value}'")
+
+
+# inpute = "[color=red test=10]".split(" ")
+def parse_metadata(metadata: str) -> dict:
+    result: dict = {
+        "zone": ZoneType.Normal,
+        "color": None,
+        "max_drones": 1,
+    }
+    if metadata.count("[") != 1 or metadata.count("]") != 1:
+        raise ValueError("Metadata not valid")
+    metadata = metadata.strip("[]")
+    for data in metadata.split(" "):
+        tag, value = data.split("=")
+        result.update({tag: valid_metadata(tag, value)})
     return result
 
 
-def parse_hub(hub: str):
-    pass
+def parse_hub(hub_line: str):
+
+    result: dict = {}
+    fields = hub_line.split(":")
+    hub_data = fields[1].split(" ")
+
+    if fields[0] == "start_hub":
+        result["type"] = "start_hub"
+
+    elif fields[0] == "hub":
+        result["type"] = "hub"
+
+    elif fields[0] == "end_hub":
+        result["type"] = "end_hub"
+
+    result.update(
+        {
+            "name": hub_data[1].strip(),
+            "x": int(hub_data[2]),
+            "y": int(hub_data[3]),
+            "matadata": parse_metadata(hub_data[4:]),
+        }
+    )
+    return result
+
+
+try:
+    print(
+        "a=",
+        parse_metadata("[color=red zone=restricted max_drones=20]"),
+        file=sys.stderr,
+    )
+except Exception as e:
+    print("# Error Parsing:", e, file=sys.stderr)
+exit()
 
 
 def parse_connection(data: str) -> dict:
@@ -32,34 +111,25 @@ def parse_connection(data: str) -> dict:
 
     return result
 
+def main():
+    lines = []
+    try:
+        with open(sys.argv[1]) as f:
+            for line in f:
+                lines.append(line)
+    except Exception as e:
+        print(e)
 
-lines = []
-try:
-    with open(sys.argv[1]) as f:
-        for line in f:
-            lines.append(line)
-except Exception as e:
-    print(e)
 
+    for line in lines:
+        line = line.split(":")
+        if line[0] == "nb_drones":
+            map_data["nb_drones"] = int(line[1].strip())
+        elif "hub" in line[0]:
+            pass
+        elif line[0] == "connection":
+            map_data["connection"].append(parse_connection(line[1]))
 
-for line in lines:
-    line = line.split(":")
-    if line[0] == "nb_drones":
-        map_data["nb_drones"] = int(line[1].strip())
-    elif "hub" in line[0]:
-        line_info = line[1].strip().split(" ")
-        new_data = {
-            "name": line_info[0],
-            "x": int(line_info[1]),
-            "y": int(line_info[2]),
-            "matadata": parse_metadata(line_info[3:]),
-        }
-        if line[0] == "start_hub" or line[0] == "end_hub":
-            map_data[line[0]] = new_data
-        elif line[0] == "hub":
-            map_data[line[0]].append(new_data)
-    elif line[0] == "connection":
+    print("test =", map_data, file=sys.stderr)
 
-        map_data["connection"].append(parse_connection(line[1]))
-
-print("test =", map_data, file=sys.stderr)
+if 
