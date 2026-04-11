eebce12141c96e4a0aa85b29ffff56ed37101c91
saad atifi <saadworkdotcom@gmail.com>
2026-04-11 14:13:27 +0100

    fix parsing to pass all levels

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from enum import Enum
+from pprint import pprint
 
 
 class ZoneType(Enum):
@@ -14,118 +15,174 @@ class Color(Enum):
     blue = "blue"
     gray = "gray"
     green = "green"
+    orange = "orange"
+    yellow = "yellow"
+    cyan = "cyan"
+    purple = "purple"
+    brown = "brown"
+    lime = "lime"
+    magenta = "magenta"
+    gold = "gold"
+    black = "black"
+    maroon = "maroon"
+    darkred = "darkred"
+    violet = "violet"
+    crimson = "crimson"
+    rainbow = "rainbow"
 
 
-def valid_metadata(tag: str, value: str):
+def valid_metadata(tag: str, value: str, connection: bool):
 
-    PARSERS = {
-        "zone": {
-            "normal": ZoneType.Normal,
-            "blocked": ZoneType.Blocked,
-            "restricted": ZoneType.Restricted,
-            "priority": ZoneType.Priority,
-        },
-        "color": {
-            "red": Color.red,
-            "blue": Color.blue,
-            "gray": Color.gray,
-            "green": Color.green,
-        },
-    }
+    PARSERS: dict = (
+        {
+            "zone": {
+                "normal": ZoneType.Normal,
+                "blocked": ZoneType.Blocked,
+                "restricted": ZoneType.Restricted,
+                "priority": ZoneType.Priority,
+            },
+            "color": {
+                "red": Color.red,
+                "blue": Color.blue,
+                "gray": Color.gray,
+                "green": Color.green,
+                "orange": Color.orange,
+                "yellow": Color.yellow,
+                "cyan": Color.cyan,
+                "purple": Color.purple,
+                "brown": Color.brown,
+                "lime": Color.lime,
+                "magenta": Color.magenta,
+                "gold": Color.gold,
+                "black": Color.black,
+                "maroon": Color.maroon,
+                "darkred": Color.darkred,
+                "violet": Color.violet,
+                "crimson": Color.crimson,
+                "rainbow": Color.rainbow,
+            },
+        }
+        if not connection
+        else {}
+    )
 
     try:
-        if tag == "max_drones":
+        if tag == "max_link_capacity" and connection:
+            return int(value)
+        if tag == "max_drones" and not connection:
             return int(value)
         return PARSERS[tag][value]
-    except KeyError:
+    except Exception:
         raise ValueError(f"tag or value not valid '{tag} = {value}'")
 
 
 # inpute = "[color=red test=10]".split(" ")
-def parse_metadata(metadata: str) -> dict:
-    result: dict = {
-        "zone": ZoneType.Normal,
-        "color": None,
-        "max_drones": 1,
-    }
+def parse_metadata(metadata: str, connection: bool = False) -> dict:
+    result: dict = {}
+
+    result = (
+        {"max_link_capacity": 1}
+        if connection
+        else {
+            "zone": ZoneType.Normal,
+            "color": None,
+            "max_drones": 1,
+        }
+    )
+
     if metadata.count("[") != 1 or metadata.count("]") != 1:
         raise ValueError("Metadata not valid")
+
     metadata = metadata.strip("[]")
+
     for data in metadata.split(" "):
+
         splitted_data = data.split("=")
         if len(splitted_data) != 2:
             raise ValueError("Metadata not valid")
+
         tag, value = splitted_data
-        result.update({tag: valid_metadata(tag, value)})
+        result.update({tag: valid_metadata(tag, value, connection)})
+
     return result
 
 
-def parse_hub(hub_line: str):
+def parse_hub(line: str):
 
-    print(hub_line)
     result: dict = {}
-    hub_data = hub_line.split(":")
-    fields = hub_data[1].strip().split(" ", 3)
+    splitted_hub = line.split(":")
+    fields = splitted_hub[1].strip().split(" ", 3)
 
-    if hub_data[0] == "start_hub":
+    if splitted_hub[0] == "start_hub":
         result["type"] = "start_hub"
 
-    elif hub_data[0] == "hub":
+    elif splitted_hub[0] == "hub":
         result["type"] = "hub"
 
-    elif hub_data[0] == "end_hub":
+    elif splitted_hub[0] == "end_hub":
         result["type"] = "end_hub"
 
     else:
         raise ValueError(f"Hub type not valid '{fields[0]}'")
 
-    x = fields[1]
-    y = fields[2]
-
-    if not x.isdecimal() or not y.isdecimal():
-        raise ValueError(f"Hub not valid")
+    try:
+        x = int(fields[1])
+        y = int(fields[2])
+    except Exception:
+        raise ValueError("Hub coordinates not valid")
     result.update(
         {
             "name": fields[0],
-            "x": int(x),
-            "y": int(y),
+            "x": x,
+            "y": y,
             "matadata": parse_metadata(fields[3]),
         }
     )
-    print("OK:", result)
     return result
 
 
-def parse_connection(data: str) -> dict:
-    result: dict = {}
-    data = data.strip()
+def parse_connection(line: str) -> dict:
+    result: dict = {"metadata": {"max_link_capacity": 1}}
+    splitted_connection = line.split(":")
+    data = splitted_connection[1].strip()
+    print(data, file=sys.stderr)
     data_list = data.split(" ")
     hubs = data_list[0].split("-")
     result["from"] = hubs[0]
     result["to"] = hubs[1]
     if len(data_list) > 1:
-        result["metadata"] = parse_metadata(data_list[1])
-
+        result["metadata"] = parse_metadata(data_list[1], True)
     return result
 
 
 def main():
-    map_data = {}
+    map_data: dict = {
+        "hubs": [],
+        "connections": [],
+    }
     lines = []
-    error_number = 0
+    line_idx = 0  # line that error on
+    hub_idx = 0
+    connection_idx = 0
+
+    # ================== open the file =================
     try:
         with open(sys.argv[1]) as f:
             for line in f:
                 lines.append(line)
     except Exception as e:
         print(e)
+    # ==================================================
 
     try:
-        for i, line in enumerate(lines):
-            print(line)
+        for line in lines:
+            line_idx += 1
+
+            # skip empty line or command '#'
             if line == "\n" or line.startswith("#"):
                 continue
-            error_number = i + 1
+
+            # split
             splitted_line = line.split(":")
 
             if len(splitted_line) != 2:
@@ -135,15 +192,16 @@ def main():
                 map_data["nb_drones"] = int(splitted_line[1].strip())
 
             elif "hub" in splitted_line[0]:
-                map_data[f"hub_{i + 1}"] = parse_hub(line)
+                map_data["hubs"].append(parse_hub(line))
 
-            # elif line[0] == "connection":
-            #     map_data["connection"].append(parse_connection(line))
+            elif splitted_line[0] == "connection":
+                map_data["connections"].append(parse_connection(line))
             else:
                 pass  # add unkownn type error later
-        print("a=", map_data, file=sys.stderr)
+
+        pprint(map_data, sort_dicts=False)
     except Exception as e:
-        print(f"Parsing error: {e} [line: {error_number}]")
+        print(f"Parsing error: {e} [line: {line_idx}]")
     return map_data
 
 
--- output.py
+++ output.py
@@ -1,1 +1,14 @@
-a= {'nb_drones': 2, 'hub_4': {'type': 'start_hub', 'name': 'start', 'x': 0, 'y': 0, 'matadata': {'zone': <ZoneType.Restricted: 'restricted'>, 'color': <Color.green: 'green'>, 'max_drones': 1}}}
+a = {
+    "nb_drones": 2,
+    "hub_5": {
+        "type": "start_hub",
+        "name": "start",
+        "x": 0,
+        "y": 0,
+        "matadata": {
+            'zone': <ZoneType.Restricted: 'restricted'>,
+            'color': <Color.green: 'green'>,
+            'max_drones': 1,
+        },
+    },
+}
