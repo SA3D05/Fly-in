b9c62373976f627f24b421383df1bf70ee5ad8a5
Saad Atifi <satifi@e2r4p13.1337.ma>
2026-05-16 14:54:44 +0100

    WIP: fixing parsing

--- parser.py
+++ parser.py
@@ -41,21 +41,33 @@ class Parser:
             return False
         return True
 
+    def __is_hub_define(self, hubs: list, hub_from: str, hub_to: str):
+        hub_from_found = False
+        hub_to_found = False
+
+        for hub in hubs:
+            if hub["name"] == hub_from:
+                hub_from_found = True
+            elif hub["name"] == hub_to:
+                hub_to_found = True
+        if hub_from_found and hub_to_found:
+            return True
+        return False
+
     def __validate_line(self, line: str) -> str | None:
-        number = r"(\-|\+){,1}[0-9]+"
-
+        number = r"(\-|\+){0,1}[0-9]+"
         name = r"[^\-\s]+"
-        kv = r"[a-z\_]+\=([a-z]+|[0-9]+)"
-        metadata = rf"(\[{kv}(\s{kv})*\]){{,1}}"
+        tag_value = r"[a-z\_]+\=([a-z]+|[0-9]+)"
+        metadata = rf"( \[{tag_value}( {tag_value})*\]){{0,1}}"
 
-        hub_pattern = rf"\s{name}\s{number}\s{number}\s{metadata}"
+        hub_pattern = rf"{name} {number} {number}{metadata}"
 
-        start_hub_pattern = rf"^start_hub:{hub_pattern}"
-        end_hub_pattern = rf"^end_hub:{hub_pattern}"
-        regular_hub_pattern = rf"^hub:{hub_pattern}"
+        start_hub_pattern = rf"^start_hub: {hub_pattern}"
+        end_hub_pattern = rf"^end_hub: {hub_pattern}"
+        regular_hub_pattern = rf"^hub: {hub_pattern}"
 
-        connection_pattern = rf"^connection:\s{name}\-{name}\s{metadata}"
-        nb_drones_pattern = rf"^nb_drones:\s{number}"
+        connection_pattern = rf"^connection: {name}\-{name}{metadata}"
+        nb_drones_pattern = rf"^nb_drones: {number}"
 
         if re.search(nb_drones_pattern, line):
             return "nb_drones"
@@ -87,6 +99,7 @@ class Parser:
         lines = self.__get_lines(filename)
         line_idx: int | None = 0
         pass_first_line = False
+        pass_defined_hubs = False
         result: dict = {
             "connections": [],
             "hubs": [],
@@ -111,7 +124,7 @@ class Parser:
                 line_content: str = splitted_line[1].strip()
 
                 if line_type == "nb_drones":
-                    if pass_first_line == True:
+                    if pass_first_line:
                         raise ValueError("invalide configuration")
                     pass_first_line = True
                     try:
@@ -120,23 +133,36 @@ class Parser:
                         raise ValueError("invalide configuration")
 
                 elif line_type == "start_hub":
+                    if pass_defined_hubs:
+                        raise ValueError("invalide configuration")
                     pass_first_line = True
                     result["hubs"].append(self.__parse_hub(line_content))
                     result["hubs"][-1].update({"type": "start_hub"})
 
                 elif line_type == "end_hub":
+                    if pass_defined_hubs:
+                        raise ValueError("invalide configuration")
                     pass_first_line = True
                     result["hubs"].append(self.__parse_hub(line_content))
                     result["hubs"][-1].update({"type": "end_hub"})
 
                 elif line_type == "hub":
+                    if pass_defined_hubs:
+                        raise ValueError("invalide configuration")
                     pass_first_line = True
                     result["hubs"].append(self.__parse_hub(line_content))
                     result["hubs"][-1].update({"type": "normal_hub"})
 
                 elif line_type == "connection":
                     pass_first_line = True
-                    result["connections"].append(self.__parse_connection(line_content))
+                    pass_defined_hubs = True
+                    connection = self.__parse_connection(line_content)
+
+                    if not self.__is_hub_define(
+                        result["hubs"], connection["hub_from"], connection["hub_to"]
+                    ):
+                        raise ValueError("invalide configuration")
+                    result["connections"].append(connection)
 
             if not self.__validate_hubs(result["hubs"]):
                 line_idx = None
@@ -175,8 +201,8 @@ class Parser:
 
     def __parse_connection(self, line_content: str) -> dict:
 
-        fields = line_content.split(" ", 1)
-        hubs = line_content.split("-")
+        fields = line_content.split(" ")
+        hubs = fields[0].split("-")
 
         if hubs[0] == hubs[1]:
             raise ValueError("Metadata not valid")
@@ -188,7 +214,6 @@ class Parser:
 
         if len(fields) > 1:
             result.update(self.__parse_connection_metadata(fields[1]))
-
         return result
 
     def __parse_hub_metadata(self, metadata: str) -> dict[str, ZoneType | str | int]:
@@ -244,6 +269,10 @@ class Parser:
             raise ValueError("Metadata not valid")
 
         splitted_data = fields[0].split("=")
+
+        if len(splitted_data) != 2:
+            raise ValueError("Metadata not valid")
+
         tag: str = splitted_data[0]
         value: str = splitted_data[1]
 
